90768c97827a9184e84042dcc50771574371f582
didier <didier@d-defrance.fr>
2020-10-12 14:41:51 +0200

    employe

--- tp-ts/src/personne.ts
+++ tp-ts/src/personne.ts
@@ -1,8 +1,8 @@
 class Personne{
-    constructor(public prenom : string  = "?",
-        public nom : string  = "?",
-        private _age : number= 0){
-        }
+    constructor(public prenom : string  = undefined,
+               public nom : string  = undefined,
+               private _age : number= 0){
+              }
 
         public get age(): number {
             return this._age;
@@ -14,6 +14,20 @@ class Personne{
             }else throw "age négatif invalid";
         }
 }
+
+
+class Employe extends Personne{
+    numero:number=0;
+    salaire:number = 0;
+    constructor(prenom:string="?",nom:string="?",age:number=0){
+        super(prenom, nom, age);
+    }
+}
+
+let e1= new Employe("alex",'Therieur',33);
+e1.numero=1;
+e1.salaire=2000;
+console.log(JSON.stringify(e1));
 
 let p1 = new Personne("jean","Bon",30);
 console.log("nom=" +p1.nom);
