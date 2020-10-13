fbb89bdce0919121211493ab01f0c15842b9adc2
didier <didier@d-defrance.fr>
2020-10-13 09:38:54 +0200

    %

--- my-app/src/app/basic/tva/tva.component.ts
+++ my-app/src/app/basic/tva/tva.component.ts
@@ -10,13 +10,13 @@ export class TvaComponent implements OnInit {
   ht : number ;
   tauxTva : number = 20; // en %
   tva : number;
-  ttc : number;
+  ttc : number
 
-  listeTaux : number[] = [ 5 , 10 , 20];
+  listeTaux : number[] = [ 5 , 10 , 20]
 
   onCalculerTvaEtTtc(){
-    this.tva = this.ht * this.tauxTva / 100;
-    this.ttc = Number(this. ht) + Number(this.tva);
+    this.tva = this.ht * this.tauxTva / 100
+    this.ttc = Number(this.ht) + Number(this.tva)
   }
 
   constructor() { }
